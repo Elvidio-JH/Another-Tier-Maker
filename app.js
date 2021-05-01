ba9922d8144aa6f00edeeab7a7e0c59881976cce
IAmSharcBait <IAmSharcBait@github.com>
2021-05-01 11:04:09 -0700

    Added a dark theme button in 'index.html'. Defined the button color in 'main.css'. Gave the button functionality in 'app.js'.

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
 /* apps.js will be used for interactivity of the site */
+'use strict'
+
+const switcher = document.querySelector('.btn'); /* gets the reference to the theme changing button */
+
+switcher.addEventListener('click', function() {
+    document.body.classList.toggle('dark-theme')
+    
+    var className = document.body.className;
+    if(className == "light-theme") {
+        this.textContent = "Dark";
+    }
+    else {
+        this.textContent = "Light";
+    }
+
+    console.log('current class name: ' + className); /* adds a message to the console in the developer tools */
+});
