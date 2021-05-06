e8c87e60f1f8b7ce100db9efc1d4ec2fb10cf787
IAmSharcBait <IAmSharcBait@github.com>
2021-05-05 20:33:55 -0700

    Created the 'tier' section and the add/delete row buttons. Added the addRow and deleteRow fucntions to 'app.js'. Added the icon tray below the tier section. Browse button no longer works and will be fixed later.

--- README.md
+++
@@ -1,4 +0,0 @@
-# AnotherTier-Maker
-A website to rank things in a tier list
-
-This project is to familiarze myself with html/css/javascript and to ultimately recreate the tier list maker website
--- app.js
+++ app.js
@@ -2,6 +2,8 @@
 'use strict'
 
 const switcher = document.querySelector('.btn'); /* gets the reference to the theme changing button */
+var num_rows = 1; 
+var srcUrls = [];
 
 switcher.addEventListener('click', function() {
     document.body.classList.toggle('dark-theme')
@@ -16,3 +18,40 @@ switcher.addEventListener('click', function() {
 
     console.log('current class name: ' + className); /* adds a message to the console in the developer tools */
 });
+
+function upload(event){
+	var srcUrl = URL.createObjectURL(event.target.files[0]);
+
+    
+};
+
+function addRow () {
+    // create a new div element
+    num_rows = num_rows + 1;
+    const newDiv = document.createElement("div");
+    newDiv.className = "row";
+    newDiv.id = num_rows;
+    
+    const x = document.createElement("section");
+    x.className = "section";
+    x.id = "tier-name";
+    x.contentEditable = "true";
+    x.innerText = "tier"
+
+    const y = document.createElement("section");
+    y.className = "section";
+    y.id = "buttons";
+    y.innerText = "btns here"
+
+    newDiv.appendChild(x);
+    newDiv.appendChild(y);
+
+    // add the newly created element and its content into the DOM
+    document.getElementById("tierlist").appendChild(newDiv);
+};
+
+function deleteRow () {
+    var row = document.getElementById(num_rows);
+    row.remove();
+    num_rows = num_rows - 1;
+};
